dd0b7da8bedd822fb5b3617a7887367bf2a54e28
Andrew Beet <andrew.beet>
2019-01-08 18:16:32 -0500

    package documentation: created

---
+++ R/mshydradata.R
@@ -0,0 +1,16 @@
+#' mshydradata: easy manipulation of input data used in Hydra
+#'
+#'An implementation of the LeMANS model as described in the Hall et al (2006) and Rochet et al (2011) publications.
+#'
+#'\itemize{
+#'\item Manipulate input data to model
+#'\item create documentation based on input data
+#'\item creat .dat and .pin files used in running the multisecies model "Hydra"
+#'}
+#'
+#'To learn more about using \code{mshydradata}, start with the vignette: \code{browseVignettes(package="mshydradata")}
+#'
+#'
+#' @docType package
+#' @name mshydradata
+NULL
