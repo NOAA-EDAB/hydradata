27c91fe9e19406438de3e4f0c651bfbf3287dfbf
Andrew Beet <andrew.beet>
2019-01-08 14:36:51 -0500

    DESCRIPTION: depend packages for documenting data

--- R/create_documentation.R
+++ R/create_documentation.R
@@ -1,7 +1,10 @@
 #'Renders .rmd file to display all model inputs
 #'
 #'
-#'
+#'@export
 
-#rmdFile <- system.file("rmd",file = "main_produceInputFigures.Rmd",package="mshydradata")
-#rmarkdown::render(rmdFile)
+create_documentation <- function(outFile="shit.html",outDir=getwd()){
+
+  rmdFile <- system.file("rmd",file = "main_produceInputFigures.Rmd",package="mshydradata")
+  rmarkdown::render(rmdFile,output_file=outFile,output_dir = outDir)
+}
