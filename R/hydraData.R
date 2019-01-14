42dda513328f4585987e75a7a04f0a8df2fd8a41
Andrew Beet <andrew.beet>
2019-01-14 13:17:35 -0500

    hydraData.R documentation setup. not complete

---
+++ R/hydraData.R
@@ -0,0 +1,16 @@
+#' Data for Hydra data nd pin files (mshydradata)
+#'
+#'All the required information for creating data and pin files are stored in this this. This
+#'data is used to create documentation and to create dat and fin files for use in simulaion with Hydra
+#'
+#' @format A list containing 133 elements
+#' \describe{
+#'     \item{speciesList}{Character vector: names of species in model}
+#'     \item{}{}
+#'     \item{}{}
+#'}
+#'
+#'@seealso To visualize this data please run \code{create_documentation}
+#'
+#'@source Gaichas et al. + publication (currently being written up)
+"hydraData"
