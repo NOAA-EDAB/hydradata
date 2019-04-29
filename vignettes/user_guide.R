16993507bb67a82b3c6c3beb1cab0016ae20fb15
andybeet <22455149+andybeet@users.noreply.github.com>
2019-04-29 13:05:02 -0400

    updated vignette to reflect usage with set_hcr

--- vignettes/user_guide.R
+++ vignettes/user_guide.R
@@ -9,6 +9,10 @@ library(hydradata)
 inputs <- setup_default_inputs()
 str(inputs)
 
+## ----echo = TRUE---------------------------------------------------------
+newData <- set_hcr(currentData=hydraData, Nthresholds=5, minMaxThresholds=c(0.1,0.4), minMaxExploitations=c(0.04,0.4), increment=.02)
+str(newData$exploitationOptions)
+
 ## ----echo = TRUE,eval = FALSE--------------------------------------------
 #  hydraData <- create_datpin_files(inputs,hydraData)
 
