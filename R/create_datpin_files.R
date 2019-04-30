1d80d4150b699ef16bc1957cb0179d9c4b878a0b
andybeet <22455149+andybeet@users.noreply.github.com>
2019-04-30 12:37:41 -0400

    remove debugging

--- R/create_datpin_files.R
+++ R/create_datpin_files.R
@@ -76,7 +76,6 @@ print(maxRampExRate)
       #      ind <- as.numeric(p$fishery_q[,ifleet]) > 1e-29 # find all > 1e-29
       ind <- as.numeric(fEInd) > 1e-29 # find all > 1e-29
       dataList$observedEffort[ifleet+1,] <- rep(maxRampExRate/(sum(fEInd[ind])/sum(ind)),dataList$Nyrs) # Effort = ex/mean(q)
-print(rep(maxRampExRate/(sum(fEInd[ind])/sum(ind)),dataList$Nyrs))
     }
     #SMALL MESH OVERWITE. EVENTUALLY REMOVE THIS
     print("********************* SMALL MESH & gillnet HARD CODED ****************************")
@@ -94,7 +93,6 @@ print(rep(maxRampExRate/(sum(fEInd[ind])/sum(ind)),dataList$Nyrs))
     stop(paste("Can not have a historical run > ",dataList$Nyrs,"years. Not enough data!"))
   }
 
-  print(options)
   # updata data based on options
   dataList <- modifyList(dataList,options)
 
