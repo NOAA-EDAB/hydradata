7a9a7c574626e4f3eef5346b52c563f3836e4750
andybeet <22455149+andybeet@users.noreply.github.com>
2019-05-08 14:28:30 -0400

    package name change

--- R/hydraData.R
+++ R/hydraData.R
@@ -1,4 +1,4 @@
-#' mshydradata: Manipulation of input data used in Hydra
+#' hydradata: Manipulation of input data used in Hydra
 #'
 #' Tools to aid in quickly changing and documenting input parameters to Hydra
 #'
@@ -8,7 +8,7 @@
 #'\item creat .dat and .pin files used in running the multisecies model "Hydra"
 #'}
 #'
-#'To learn more about using \code{mshydradata}, start with the vignette: \code{browseVignettes(package="mshydradata")} or click \code{index} link below
+#'To learn more about using \code{hydradata}, start with the vignette: \code{browseVignettes(package="hydradata")} or click \code{index} link below
 #'
 #'
 #' @docType package
